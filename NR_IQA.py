7f9936a5fdd1867b4b9b50a3c79c1560c30b2554
Dimitry Pianykh <dimitryolegpianykh@gmail.com>
2023-07-09 00:48:24 +0300

    test commit

---
+++ NR_IQA.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 23 21:29:19 2023
+
+@author: ddude
+"""
+
+import numpy as np
+import time
+from scipy import signal
+st = time.time()
+"""
+BLUR
+"""
+import pydicom
+
+import matplotlib.pyplot as plt
+
+
+def show_ct_dicom(path):
+    #convert from houndsfield to pixel
+    dicom = pydicom.read_file(path)
+    data = dicom.pixel_array   
+    data = data - dicom.RescaleIntercept
+    data = data / dicom.RescaleSlope
+    data = (data * 255).astype(np.uint8)
+    bound = int((np.shape(data)[0]-(np.shape(data)[0]/np.sqrt(2)))/2)  
+    return data[(bound-2):-(bound+2), (bound-2):-(bound+2)]
+    # return data
+
+
+
+def Blur_Statistics(img):
+    harr = np.array([[0, 0, 0],[-1, 0, 1], [0, 0, 0]])
+    varr = np.array([[0, -1, 0], [0, 0, 0], [0, 1, 0]])
+    D_h = abs(signal.convolve2d(img, harr))
+    D_v = abs(signal.convolve2d(img, varr))
+    
+    D_hm = np.average(D_h)
+    D_vm = np.average(D_v)
+    
+    C_h = np.where(D_h>D_hm, D_h, 0)
+    C_v = np.where(D_v>D_vm, D_v, 0)
+    
+    E_h = np.zeros_like(C_h)
+    E_v = np.zeros_like(C_v)
+    E_h[2:-2, 2:-2] = ((C_h[2:-2, 2:-2]>C_h[2:-2, 1:-3]) & (C_h[2:-2, 2:-2]>C_h[2:-2, 3:-1]))
+    E_v[2:-2, 2:-2] = ((C_h[2:-2, 2:-2]>C_h[1:-3, 2:-2]) & (C_h[2:-2, 2:-2]>C_h[3:-1, 2:-2]))
+    E_h = E_h[2:-2, 2:-2]
+    E_v = E_v[2:-2, 2:-2]
+    
+    Th_b = 0.1
+    
+    D_h = D_h[2:-2, 2:-2]
+    D_v = D_v[2:-2, 2:-2]
+    A_h = D_h/2
+    A_v = D_v/2
+    
+    data = np.copy(img)
+    data = data[1:-1, 1:-1]
+    
+    Br_h = np.where(A_h != 0, (abs(data-A_h))/A_h, Th_b)
+    Br_v = np.where(A_v != 0, (abs(data-A_v))/A_v, Th_b)
+    BR = np.maximum(Br_h, Br_v)
+    
+    
+    
+    blur_sum = np.sum(np.where(BR < Th_b, BR , 0))
+    blur_count = np.sum(BR < Th_b)
+    edge_count = np.sum(E_h) + np.sum(E_v == E_h, where = 1)
+    #Blur Mean, Blur Ratio
+    return(blur_sum/blur_count, blur_count/edge_count)
+
+"""
+NOISE
+"""
+
+def Noise_Statistics(img):
+    
+    farr = ([[1, 1, 1], [1, 1, 1], [1, 1, 1]])
+    fimg = signal.convolve2d(img, farr)/9
+    
+    harr = np.array([[0, 0, 0],[-1, 0, 1], [0, 0, 0]])
+    varr = np.array([[0, -1, 0], [0, 0, 0], [0, 1, 0]])
+    D_h = abs(signal.convolve2d(fimg, harr))
+    D_v = abs(signal.convolve2d(fimg, varr))  
+    
+    D_hm = np.average(D_h)
+    D_vm = np.average(D_v)
+    
+    N_cand = np.where((D_h<=D_hm) & (D_v<=D_vm), np.maximum(D_h, D_v) , 0)
+    N_candm = np.average(N_cand)
+    
+    N = np.where(N_cand>N_candm, N_cand, 0)
+    noise_sum = np.average(N)
+    noise_count = np.sum(N_cand>N_candm)
+    #noise mean and noise ratio
+    return (noise_sum/noise_count, noise_count/(np.shape(img)[0]*np.shape(img)[1]))
+
+# """
+# COMBINATION OF BLUR AND NOISE
+
+# """
+
+def blur_noise_metric(img):
+    #optimized values from study 
+    w1 = 1
+    w2 = 0.95
+    w3 = 0.3
+    w4 = 0.75
+    blur_mean = Blur_Statistics(img)[0]
+    blur_ratio = Blur_Statistics(img)[1]
+    noise_mean = Noise_Statistics(img)[0]
+    noise_ratio = Noise_Statistics(img)[1]
+    print(f"Blur Mean:{blur_mean}\nBlur Ratio: {blur_ratio}\nNoise Mean: {noise_mean}\nNoise Ratio: {noise_ratio} ")
+    return (f"BLUR AND NOISE SCORE: {1-(w1*blur_mean+w2*blur_ratio+w3*noise_mean+w4*noise_ratio)}")
+
+img = show_ct_dicom("t1.dcm")
+plt.figure(figsize = (13,13))
+plt.imshow(img, 'gray')
+print(blur_noise_metric(img))
+    
+
+
+et = time.time()
+print(f"Runtime: {et-st}")
